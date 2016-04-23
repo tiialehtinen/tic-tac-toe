e3ef378bd8dc0b3dbe37b9ffd2ef514ef6353a4a
Daniel <danijel.maricic@gmail.com>
2016-04-23 19:37:20 +0300

    added matrix

--- js/main.js
+++ js/main.js
@@ -4,34 +4,84 @@
 
 // Our X Player
 var xPlayer = 'X';
+var xPlayerTurns = 0;
+var xPlayerPositions = [];
 
 // Our O Player
 var oPlayer = 'O';
+var oPlayerTurns = 0;
+var oPlayerPositions = [];
 
 // Who is our first player
 var currentPlayer = oPlayer;
 
+// Minimum of turns for a winner
+var minTurnsForWinner = 5; // this equals the sum of oPlayerTurns + xPlayerTurns
+
+var winner = false;
+
+
 console.log('Our first player is: ' + currentPlayer);
 
 // tdEl=table data element
-function setValue(tdEl) {
-  if( tdEl.innerText != '' ) {
+function setValue(tdEl, event) {
+
+  var xAxisObject = event.target.dataset; // this is table cell (TD)
+  var yAxisObject = event.target.parentNode.dataset; // this is parent TR
+
+  // This shall be obayed, [xAxis,yAxis]
+  var fullCoord = [ parseInt(xAxisObject.value, 10), parseInt(yAxisObject.value, 10)]
+  console.log('currentPlayer',currentPlayer,fullCoord)
+
+  if( tdEl.innerText != '' && !winner ) {
     alert('You can not click here!!!');
     return;
   }
   //setting the value of the currentplayer to the tdEl
   tdEl.innerText = currentPlayer;
 
+
+  setPlayerTurn(currentPlayer)
+
+  // we should check for winner or not
+  if( shouldWeCheckForWinner() && isPlayerWinner( currentPlayer ) ) {
+    return winner;
+  }
+
   var _nextCurrentPlayer = getNextPlayerFromCurrentPlayer();
 
-  console.log('Our currentPlayer is: ' + currentPlayer);
-  console.log('Our nextPlayer is: ' + _nextCurrentPlayer);
+  console.log('xPlayer', xPlayer, xPlayerTurns);
+  console.log('oPlayer', oPlayer, oPlayerTurns);
 
-  console.log('Setting currentPlayer from nextPlayer: ');
+
+  // we are doing this last, player might WIN in this turn
   setCurrentPlayer(_nextCurrentPlayer);
-  
+
   // this disables onclick event for a field that has a value
   tdEl.style.pointerEvents = 'none'
+}
+
+function setPlayerTurn(player) {
+  if( player === xPlayer ){
+    xPlayerTurns += 1;
+  } else {
+    oPlayerTurns +=1 ;
+  }
+}
+
+// This returns true if there is more than 5 turns
+// or false if less than 5 turns
+// default output is FALSE
+function shouldWeCheckForWinner() {
+    var shouldCheck = xPlayerTurns + oPlayerTurns;
+    if( shouldCheck >= minTurnsForWinner ) {
+      return true;
+    }
+    return false;
+}
+
+function isPlayerWinner() {
+
 }
 
 function getNextPlayerFromCurrentPlayer() {
