4fe77f8f9efce874518578931406b82ff184e0a6
Daniel <danijel.maricic@gmail.com>
2016-04-24 16:27:31 +0300

    works to a point

--- js/main.js
+++ js/main.js
@@ -1,100 +1,138 @@
-/////////////////////////
-// Our init of the app //
-/////////////////////////
+// document.addEventListener("DOMContentLoaded", function() {
+    // code...
 
-// Our X Player
-var xPlayer = 'X';
-var xPlayerTurns = 0;
-var xPlayerPositions = [];
+    /////////////////////////
+    // Our init of the app //
+    /////////////////////////
 
-// Our O Player
-var oPlayer = 'O';
-var oPlayerTurns = 0;
-var oPlayerPositions = [];
+    // Our X Player
+    var xPlayer = 'X';
+    var xPlayerTurns = 0;
+    var xPlayerPositions = [];
 
-// Who is our first player
-var currentPlayer = oPlayer;
+    // Our O Player
+    var oPlayer = 'O';
+    var oPlayerTurns = 0;
+    var oPlayerPositions = [];
 
-// Minimum of turns for a winner
-var minTurnsForWinner = 5; // this equals the sum of oPlayerTurns + xPlayerTurns
+    // Who is our first player
+    var currentPlayer = oPlayer;
 
-var winner = false;
+    // Minimum of turns for a winner
+    var minTurnsForWinner = 5; // this equals the sum of oPlayerTurns + xPlayerTurns
+
+    var winner = false;
+    var winCombinations = [ [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6] ];
+
+    // tdEl=table data element
+    function setValue(tdEl, event) {
+
+        if(winner){
+          alert('We have a winner already');
+          return;
+        }
+        var position = event.target.dataset.value;
+        if (tdEl.innerText !== '' && !winner) {
+            alert('You can not click here!!!');
+            return;
+        }
+        //setting the value of the currentplayer to the tdEl
+        tdEl.innerText = currentPlayer;
+
+        setPlayerPosition(position);
+        setPlayerTurn(currentPlayer);
+
+        // we should check for winner or not
+        if (shouldWeCheckForWinner() && isPlayerWinner(currentPlayer)) {
+          alert('You are the winner');
+            winner = true;
+        }
+
+        var _nextCurrentPlayer = getNextPlayerFromCurrentPlayer();
+
+        // console.log('xPlayer', xPlayer, xPlayerTurns, xPlayerPositions);
+        // console.log('oPlayer', oPlayer, oPlayerTurns, oPlayerPositions);
 
 
-console.log('Our first player is: ' + currentPlayer);
+        // we are doing this last, player might WIN in this turn
+        setCurrentPlayer(_nextCurrentPlayer);
 
-// tdEl=table data element
-function setValue(tdEl, event) {
-
-  var xAxisObject = event.target.dataset; // this is table cell (TD)
-  var yAxisObject = event.target.parentNode.dataset; // this is parent TR
-
-  // This shall be obayed, [xAxis,yAxis]
-  var fullCoord = [ parseInt(xAxisObject.value, 10), parseInt(yAxisObject.value, 10)]
-  console.log('currentPlayer',currentPlayer,fullCoord)
-
-  if( tdEl.innerText != '' && !winner ) {
-    alert('You can not click here!!!');
-    return;
-  }
-  //setting the value of the currentplayer to the tdEl
-  tdEl.innerText = currentPlayer;
-
-
-  setPlayerTurn(currentPlayer)
-
-  // we should check for winner or not
-  if( shouldWeCheckForWinner() && isPlayerWinner( currentPlayer ) ) {
-    return winner;
-  }
-
-  var _nextCurrentPlayer = getNextPlayerFromCurrentPlayer();
-
-  console.log('xPlayer', xPlayer, xPlayerTurns);
-  console.log('oPlayer', oPlayer, oPlayerTurns);
-
-
-  // we are doing this last, player might WIN in this turn
-  setCurrentPlayer(_nextCurrentPlayer);
-
-  // this disables onclick event for a field that has a value
-  tdEl.style.pointerEvents = 'none'
-}
-
-function setPlayerTurn(player) {
-  if( player === xPlayer ){
-    xPlayerTurns += 1;
-  } else {
-    oPlayerTurns +=1 ;
-  }
-}
-
-// This returns true if there is more than 5 turns
-// or false if less than 5 turns
-// default output is FALSE
-function shouldWeCheckForWinner() {
-    var shouldCheck = xPlayerTurns + oPlayerTurns;
-    if( shouldCheck >= minTurnsForWinner ) {
-      return true;
+        // this disables onclick event for a field that has a value
+        tdEl.style.pointerEvents = 'none';
     }
-    return false;
-}
 
-function isPlayerWinner() {
+    function setPlayerTurn(player) {
+        if (player === xPlayer) {
+            xPlayerTurns += 1;
+        } else {
+            oPlayerTurns += 1;
+        }
+    }
 
-}
+    function setPlayerPosition(position) {
+      position = parseInt(position,10);
+      if (currentPlayer === xPlayer) {
+            xPlayerPositions.push(position);
+            xPlayerPositions.sort();
+        } else {
+            oPlayerPositions.push(position);
+            oPlayerPositions.sort();
+        }
+    }
 
-function getNextPlayerFromCurrentPlayer() {
-  var _nextCurrentPlayer = '';
 
-  if( currentPlayer === xPlayer ) {
-      _nextCurrentPlayer = oPlayer;
-  } else if ( currentPlayer === oPlayer ) {
-      _nextCurrentPlayer = xPlayer;
-  }
-  return _nextCurrentPlayer;
-}
+    // This returns true if there is more than 5 turns
+    // or false if less than 5 turns
+    // default output is FALSE
+    function shouldWeCheckForWinner() {
+        var shouldCheck = xPlayerTurns + oPlayerTurns;
+        if (shouldCheck >= minTurnsForWinner) {
+            return true;
+        }
+        return false;
+    }
 
-function setCurrentPlayer(_nextCurrentPlayer) {
-    currentPlayer = _nextCurrentPlayer;
-}
+    function isPlayerWinner() {
+      var positions = '';
+      var winner = false;
+      if (currentPlayer === xPlayer) {
+            positions = xPlayerPositions;
+        } else {
+            positions = oPlayerPositions;
+        }
+      winCombinations.map(function (combo) {
+
+        var diff = difference(combo,positions);
+        if( diff.length === 0  ){
+          winner = true;
+        }
+        
+      });
+      return winner;
+    }
+
+    function getNextPlayerFromCurrentPlayer() {
+        var _nextCurrentPlayer = '';
+
+        if (currentPlayer === xPlayer) {
+            _nextCurrentPlayer = oPlayer;
+        } else if (currentPlayer === oPlayer) {
+            _nextCurrentPlayer = xPlayer;
+        }
+        return _nextCurrentPlayer;
+    }
+
+    function setCurrentPlayer(_nextCurrentPlayer) {
+        currentPlayer = _nextCurrentPlayer;
+    }
+
+    function difference(a1, a2) {
+      var result = [];
+      for (var i = 0; i < a1.length; i++) {
+        if (a2.indexOf(a1[i]) === -1) {
+          result.push(a1[i]);
+        }
+      }
+      return result;
+    }
+// });
