84d2ba29da60d6d1a32433a914d421aeec52240e
woss <danijel.maricic@gmail.com>
2016-05-10 10:05:59 +0300

    refactor in favour of the App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,22 +6,65 @@ import Tic from '../Tic/Tic'
 export default class App extends React.Component {
     constructor(props) {
         super(props)
+
+        this.state = {
+            currentPlayer: {},
+            winner: null,
+            tic: {
+                clicked: false,
+                value: '+'
+            }
+        }
     }
 
-    generateTics() {
-        var Tics = []
-        for (var i = 1; i <= 9; i++) {
-            Tics.push(<Tic key={i} id={i}/>)
-        }
-        return Tics
+    setPlayer(player) {
+        this.setState({
+            clicked: true,
+            // value: this.props.player.name
+        })
+    }
+
+    setCurrentPlayer(player) {
+        var state = this.state
+        state.currentPlayer = player
+        this.setState(state)
     }
 
     render() {
-        var _Tics = this.generateTics()
-        console.log(this)
+        var Tics = []
+
+        var _Players = [
+            {
+                id: 1,
+                name: 'X',
+                wins: 0,
+                positions: []
+            },
+            {
+                id: 2,
+                name: 'O',
+                wins: 0,
+                positions: []
+            }
+        ]
+
+        for (var i = 1; i <= 9; i++) {
+            Tics.push(<Tic
+                key={i}
+                value='+'
+                player={() => setPlayer()}
+            />)
+        }
         return (
             <div className="main">
-                <div>{_Tics}</div>
+                <div className="tics">{Tics}</div>
+                <div>
+                    Choose a player
+                    <input onClick={() => this.setCurrentPlayer(_Players.filter(function (player) {return player.id === 1})[0])} type="radio" name="player"/> X player
+                    <input onClick={() => this.setCurrentPlayer(_Players.filter(function (player) {return player.id === 2})[0])} type="radio" name="player"/> O player
+
+                </div>
+
             </div>
         )
     }
--- src/components/Tic/Tic.js
+++ src/components/Tic/Tic.js
@@ -1,19 +1,9 @@
 import * as React from "react";
 import './_Tic.scss'
 export default class Tic extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            clicked: false
-        }
-    }
-    handleClick() {
-        this.setState({clicked:true})
-        console.log(this.state)
-    }
     render() {
         return (
-            <div className="ttt"><span onClick={() => this.handleClick()}>+</span></div>
+            <div className="ttt"><span>{this.props.value}</span></div>
         )
     }
 }
