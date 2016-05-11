e5412c92e8b3119ebd8f6e8e47392b1e5b7dcb20
woss <danijel.maricic@gmail.com>
2016-05-11 11:36:17 +0300

    layout is working

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,61 +7,126 @@ export default class App extends React.Component {
     constructor(props) {
         super(props)
 
-        this.state = {
-            currentPlayer: {},
+        var state = {
+            players: [
+                {
+                    id: 0,
+                    name: 'X',
+                    wins: 0,
+                    positions: [],
+                    turns: 0,
+                    current: false,
+                    next: false,
+                },
+                {
+                    id: 1,
+                    name: 'O',
+                    wins: 0,
+                    positions: [],
+                    turns: 0,
+                    current: false,
+                    next: false,
+                }
+            ],
             winner: null,
-            tic: {
-                clicked: false,
-                value: '+'
-            }
+            winCombinations: [
+                [0, 1, 2],
+                [3, 4, 5],
+                [6, 7, 8],
+                [0, 3, 6],
+                [1, 4, 7],
+                [2, 5, 8],
+                [0, 4, 8],
+                [2, 4, 6]
+            ]
         }
+        this.state = state
+
+        this.setCurrentPlayer(0)
+        this.setNextPlayer()
     }
 
-    setPlayer(player) {
-        this.setState({
-            clicked: true,
-            // value: this.props.player.name
+    getCurrentPlayer() {
+        var currentPlayer = this.state.players.filter(function (player) {
+            if (player.current) {
+                return player
+            }
         })
+        return currentPlayer[0]
     }
 
-    setCurrentPlayer(player) {
-        var state = this.state
-        state.currentPlayer = player
-        this.setState(state)
+    getNextPlayer() {
+        var nextPlayer = this.state.players.filter(function (player) {
+            if (player.next) {
+                return player
+            }
+        })
+        return nextPlayer[0]
+    }
+
+    handlePropagation(TicId) {
+        var currentPlayer = this.getCurrentPlayer()
+        currentPlayer.turns += 1
+        currentPlayer.positions.push(TicId)
+
+        this.savePlayer(currentPlayer)
+
+        var nextPlayer = this.getNextPlayer()
+
+        this.setCurrentPlayer(nextPlayer.id)
+        this.setNextPlayer()
+    }
+
+    setCurrentPlayer(index) {
+        this.state.players.map(function (_p) {
+            if( _p.current ) {
+                _p.current = false
+                _p.next = true
+            }
+        })
+        this.state.players[index].current = true
+        this.state.players[index].next = false
+    }
+
+    setNextPlayer() {
+        this.state.players.map(function (player) {
+            if (!player.current) {
+                player.next = !player.current
+                player.current = !player.next
+            }
+        })
+
+    }
+
+    savePlayer(player) {
+        this.state.players.map(function (_player) {
+            if (_player.id === player.id) {
+                _player = player
+            }
+        })
     }
 
     render() {
         var Tics = []
 
-        var _Players = [
-            {
-                id: 1,
-                name: 'X',
-                wins: 0,
-                positions: []
-            },
-            {
-                id: 2,
-                name: 'O',
-                wins: 0,
-                positions: []
-            }
-        ]
-
         for (var i = 1; i <= 9; i++) {
             Tics.push(<Tic
                 key={i}
-                value='+'
-                player={() => setPlayer()}
+                id={i}
+                currentPlayer={this.getCurrentPlayer.bind(this)}
+                onClick={this.handlePropagation.bind(this)}
+
             />)
         }
         return (
             <div className="main">
-                <div className="tics">{Tics}</div>
+                <div className="tics">
+                    {Tics}
+
+                </div>
                 <div>
-                    Choose a player
-                    <input onClick={() => this.setCurrentPlayer(_Players.filter(function (player) {return player.id === 1})[0])} type="radio" name="player"/> X player
-                    <input onClick={() => this.setCurrentPlayer(_Players.filter(function (player) {return player.id === 2})[0])} type="radio" name="player"/> O player
+                    <div>Current Player is <strong>{this.getCurrentPlayer().name}</strong></div>
+                    <div>Next Player is <strong>{this.getNextPlayer().name}</strong></div>
 
                 </div>
 
--- src/components/Tic/Tic.js
+++ src/components/Tic/Tic.js
@@ -1,9 +1,31 @@
-import * as React from "react";
+import React from 'react';
 import './_Tic.scss'
+
 export default class Tic extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            clicked: false,
+            value: '+'
+        }
+    }
+
+    setTicPlayer(e) {
+        var state = this.state;
+        state = {
+            clicked: !state.clicked,
+            value: this.props.currentPlayer().name
+        }
+        this.setState(state)
+        this.props.onClick(this.props.id)
+    }
+
     render() {
+        // DON'T forget to bind onClick method to itself, will fall silently
         return (
-            <div className="ttt"><span>{this.props.value}</span></div>
+            <div className="ttt">
+                <span onClick={this.setTicPlayer.bind(this)}>{this.state.value}</span>
+            </div>
         )
     }
 }
